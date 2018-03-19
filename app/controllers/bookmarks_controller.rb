37f75e55d50cb2be0a2ad2178f29a8d30c0dda90
Dan <dan@carmon.org.il>
2018-03-19 19:41:19 +0100

    Initial bookmark create,show API

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,9 +1,19 @@
 class BookmarksController < ApplicationController
+  before_action :set_bookmark_item
+
   def create
     @bookmark = Bookmark.create!(bookmark_params)
     render json: @bookmark, status: :created
   end
+
+  def show
+    render json: @bookmark
+  end
   private
+
+  def set_bookmark_item
+    @bookmark = Bookmark.find_by(id: params[:id])
+  end
 
   def bookmark_params
     bookmark = params[:bookmark]
@@ -14,7 +24,7 @@ class BookmarksController < ApplicationController
     bookmark[:path] = "#{uri.path}?#{uri.query}"
     site = Site.find_or_create_by(hostname: uri.host) # TODO: handle race condition?
     bookmark[:site_id] = site.id
-    params.require(:bookmark).permit(:title, :site_id, :path)
+    params.require(:bookmark).permit(:title, :site_id, :path, :shortening)
     # TODO: maybe check if site should be removed
   end
 end
--- spec/requests/bookmarks_api_spec.rb
+++ spec/requests/bookmarks_api_spec.rb
@@ -6,13 +6,24 @@ describe "Bookmarks API" , type: :request do
       format: :json,
       bookmark: {
         url: "hello.com/bla?s=param",
+        shortening: "go.to/1",
         title: "awesome title",
       }
     }
     post "/bookmarks", params: params
+    puts('<-DANDEBUG-> bookmarks_api_spec.rb\\ 14: json:', json)
     expect(response).to have_http_status(:created)
     expect(json[:title]).to eq("awesome title")
     expect(json[:bookmark_url]).to eq('hello.com/bla?s=param')
-    expect(json[:shortening]).to eq(nil)
+    expect(json[:shortening]).to eq("go.to/1")
+    id = json[:id]
+
+    get "/bookmarks/#{id}"
+    expect(response).to have_http_status(:ok)
+    expect(json[:title]).to eq("awesome title")
+    expect(json[:bookmark_url]).to eq('hello.com/bla?s=param')
+    expect(json[:shortening]).to eq("go.to/1")
+
+    # TODO: misc bad requests tests
   end
 end
