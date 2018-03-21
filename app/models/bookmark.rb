a72c0863e073c1965776202c884dfc1899e9252a
Dan <dan@carmon.org.il>
2018-03-21 03:43:44 +0100

    Protocol is optional in client

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,6 +3,7 @@ require './lib/url_shortener'
 class Bookmark < ApplicationRecord
   has_and_belongs_to_many :tag
   belongs_to :site
+  before_validation :add_schemes
   before_validation :register_site
   before_validation :shorten_url
   after_destroy :check_site
@@ -10,6 +11,7 @@ class Bookmark < ApplicationRecord
   validates :shortening, url: {allow_blank: true, allow_nil: true}
   validates :title, {length: {minimum: 1}}
   validates :site, presence: true
+  DEFAULT_SCHEME = "http://"
 
   def as_json(opts = {includes: :tag})
     ans = super(opts)
@@ -18,13 +20,24 @@ class Bookmark < ApplicationRecord
   end
   private
 
+  def maybe_add_scheme(url)
+    ans = url
+    uri = URI(self.url)
+    ans = DEFAULT_SCHEME + ans if uri.scheme.blank?
+    ans
+  end
+
+  def add_schemes
+    self.url = maybe_add_scheme(self.url)
+    self.shortening = maybe_add_scheme(self.shortening) unless self.shortening.blank?
+  end
+
   def register_site
     # TODO: maybe check if site should be removed (in case validation failed)
     uri = URI(self.url)
     self.site = Site.find_or_create_by(hostname: uri.host) # TODO: handle race condition?
   rescue URI::InvalidURIError
     self.site_id = nil # Will be handled by validation
-    return
   end
 
   def shorten_url
